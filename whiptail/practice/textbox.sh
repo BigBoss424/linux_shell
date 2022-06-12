2c59d0594abdeac4968d80854cfa7877b5000922
Aaron Jones <ajones061392@yahoo.com>
2022-06-12 16:54:25 -0700

    Created a textbox utilizing whiptail.

--- whiptail/practice/textbox.sh
+++ whiptail/practice/textbox.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-whiptail --title "Test Message Box" --msgbox "Create a message box with whiptail. Choose Ok to continue" 10 60
+whiptail --title "Test Message Box" 
+--msgbox "Create a message box with whiptail. 
+          Choose Ok to continue" 10 60
